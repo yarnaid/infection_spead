6e3aed6fd22ff07ec6cc29d69726ad45d8866a1d
Dmitroved <dmitroved@gmail.com>
2021-07-06 00:58:59 +0300

    add working getUpdate

--- backend/server.py
+++ backend/server.py
@@ -8,22 +8,38 @@ from dataStructure.gRPC import gRPC_status_creator
 
 class ModelingServicer(spec_pb2_grpc.ModelingServicer):
 
-    def GetUpdate(self, request, context):
-        upd = spec_pb2.UpdateResponse(meta=spec_pb2.Metadata(status=gRPC_status_creator("SUCCESS"),
-                                                             request_id=request.meta.request_id))
-        return upd
+    def __init__(self, map_obj, model_objects):  # Remember our Backend output exists
+        self.map = map_obj
+        self.model_objects = model_objects
 
-    def GetMap(self, request, context):
-        current_map = []  # One of output of our server
-        for building in current_map:
-            grpc_building = self.create_grpc_building(building, request)
+    def GetUpdate(self, request, context):  # Generator of people on modeling
+        status = spec_pb2.Metadata(status=gRPC_status_creator["SUCCESS"],
+                                   request_id=request.meta.request_id)
+        for elem in self.model_objects:
+            grpc_state = ModelingServicer.create_update_response(elem, status)
+            yield grpc_state
+
+    @staticmethod
+    def create_update_response(human, status):  # create pb2 object from backed human
+        return spec_pb2.UpdateResponse(meta=status,
+                                       state=spec_pb2.State(
+                                           id=human.id,
+                                           type=human.type,
+                                           coord_x=human.x,
+                                           coord_y=human.y
+                                       ))
+
+    def GetMap(self, request, context):  # Generator of map objects
+        status = spec_pb2.Metadata(status=gRPC_status_creator["SUCCESS"],
+                                   request_id=request.meta.request_id)
+        for building in self.map:
+            grpc_building = self.create_grpc_building(building, status)
             yield grpc_building
 
     @staticmethod
-    def create_grpc_building(building, request):
-        return spec_pb2.Map(meta=spec_pb2.Metadata(status=gRPC_status_creator("SUCCESS"),
-                                                   request_id=request.meta.request_id),
-                            building=spec_pb2.Metadata.Building(
+    def create_grpc_building(building, status):  # create pb2 object from backend building
+        return spec_pb2.Map(meta=status,
+                            building=spec_pb2.Map.Building(
                                 id=building.id,
                                 type=building.type,
                                 coord_x=building.x,
@@ -34,10 +50,10 @@ class ModelingServicer(spec_pb2_grpc.ModelingServicer):
                             ))
 
 
-def serve():
+def serve():  # Responsible for the operation of the server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     spec_pb2_grpc.add_ModelingServicer_to_server(
-        ModelingServicer(), server)
+        ModelingServicer(None, None), server)  # NEED BACKEND OUTPUT INSTEAD OF NONE
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
--- frontend/client.py
+++ frontend/client.py
@@ -1,19 +1,25 @@
 from __future__ import print_function
 from gRPC import spec_pb2_grpc, spec_pb2
-from dataStructure.gRPC import gRPC_status_creator,RequestCounter
-import random
+from dataStructure.gRPC import gRPC_status_creator, RequestCounter
 import logging
 
 import grpc
 
 
-def update_request(stub):
-    req = spec_pb2.UpdateRequest(meta=spec_pb2.Metadata(status=gRPC_status_creator("SUCCESS"),
+def update_request(stub):  # Request to update the states of modeling objects
+    req = spec_pb2.UpdateRequest(meta=spec_pb2.Metadata(status=gRPC_status_creator["SUCCESS"],
                                                         request_id=RequestCounter.give_id()))
-    feature = stub.GetUpdate(req)
+    object_generator = stub.GetUpdate(req)  # We get a generator of people in the simulation from the server
+    return object_generator
 
 
-def run():
+def get_map(stub):  # Request for getting map objects
+    req = spec_pb2.Empty()
+    map_generator = stub.Map(req)  # We received a generator of objects on the map from the server
+    return map_generator
+
+
+def run_update():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
@@ -22,6 +28,12 @@ def run():
         update_request(stub)
 
 
+def run_get_map():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = spec_pb2_grpc.ModelingStub(channel)
+        get_map(stub)
+
+
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    run_update()
