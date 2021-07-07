8da97846f19baa67d879d8df8e72a533c9ddbd74
Dmitroved <dmitroved@gmail.com>
2021-07-07 18:11:30 +0300

    clear comments

--- dataStructure/gRPC.py
+++ dataStructure/gRPC.py
@@ -4,11 +4,6 @@ from pure_protobuf.types import int32
 from enum import IntEnum
 
 
-# gRPC_status_creator = {'UNDEFINED': 0, 'SUCCESS': 1, 'ERROR': 2}  # Dont know how to do it better
-# gRPC_human_types = {'NORMAL': 0, 'ILL': 1, 'RECOVERED': 2, 'DEAD': 3}
-# gRPC_building_types = {'HOUSE': 0, 'ROAD': 1}
-
-
 class RequestCounter:  # counter of unique request id
     id = 0
 
