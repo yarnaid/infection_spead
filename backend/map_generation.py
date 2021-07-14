13c170e546a6108560b9d0a11e9b43d639ea7a87
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2021-07-14 16:49:08 +0500

    added automatic indexing instead of itertools

--- backend/map_generation.py
+++ backend/map_generation.py
@@ -19,8 +19,6 @@ class ResearchMap:
         into the city along each coordinate axis 5 houses,
         so we will store the limitation on the length of the wall as a field of the map instance,
         depending on the length / width of the random card)
-     id_counter: int
-        Counter of objects (Buildings or HumanStates) on the map
      map_population: list
         List of HumanState-objects, placed on the map
      map_buildings: list
@@ -32,11 +30,12 @@ class ResearchMap:
         self.config_data = ConfigFileParser(config_name).parse_config()
         self.__wall_len_limit = self.config_data[ConfigParameters.MAP_LENGTH.value]\
                                 // self.config_data[ConfigParameters.WALL_LENGTH_DIVIDER.value]
-        self.__id_counter = count()
         self.map_length = self.config_data[ConfigParameters.MAP_LENGTH.value]
         self.map_width = self.config_data[ConfigParameters.MAP_WIDTH.value]
-        self.__map_population = self.create_generation_list()
-        self.__map_buildings = self.create_buildings_list()
+        self.__map_population = []
+        self.__map_buildings = []
+        self.create_generation_list()
+        self.create_buildings_list()
 
     def generator_buildings(self):
 
@@ -69,7 +68,7 @@ class ResearchMap:
         buildings_list = []
         buildings_quantity = self.config_data[ConfigParameters.BUILDINGS_QUANTITY.value]
         for i in range(buildings_quantity):
-            new_building = Building.from_parameters(next(self.__id_counter),
+            new_building = Building.from_parameters(len(self.__map_buildings) + len(self.__map_population),
                                                     self.config_data[ConfigParameters.MIN_WALL_LEN.value],
                                                     self.__wall_len_limit,
                                                     self.config_data[ConfigParameters.BORDERS_INDENT.value],
@@ -79,9 +78,9 @@ class ResearchMap:
                 buildings_list.append(new_building)
             else:
                 iterations = count()
-                while new_building.has_intersection(buildings_list)\
+                while self.has_intersection(new_building)\
                         and next(iterations) < self.config_data[ConfigParameters.ITERATION_CONSTRAINT.value]:
-                    new_building = Building.from_parameters(next(self.__id_counter),
+                    new_building = Building.from_parameters(len(self.__map_buildings) + len(self.__map_population),
                                                             self.config_data[ConfigParameters.MIN_WALL_LEN.value],
                                                             self.__wall_len_limit,
                                                             self.config_data[ConfigParameters.BORDERS_INDENT.value],
@@ -99,23 +98,25 @@ class ResearchMap:
         """
         human_objects = []
         for i in range(self.config_data[ConfigParameters.POPULATION_QUANTITY.value]):
-            human_objects.append(HumanState.generate_random_human(self.length(), self.width(), next(self.__id_counter)))
+            human_objects.append(HumanState.human_from_parameters(self.length(), self.width(), len(self.__map_buildings)
+                                                                  + len(self.__map_population)))
         return human_objects
 
-    def has_intersection(self, buildings_list: typing.List[Building]):
+    def has_intersection(self, new_building: Building):
 
         """
         Method for finding intersection between current building and already existing buildings (from list)
 
-        :param buildings_list: list
-            List of Building-objects, which keeps all building, already placed on the map
+        :param: new_building: Building
+            New with which we want to drive the intersections of other objects on the map
+
         :return:
             True, if new building has an intersection with at least one building from list of placed buildings,
             or False in other cases
         """
 
-        for building in buildings_list:
-            if Building.intersection_check(building, self):
+        for building in self.__map_buildings:
+            if new_building.intersection_check(building):
                 return True
         return False
 
