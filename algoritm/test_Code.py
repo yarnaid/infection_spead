eebd2b1647213a973b1e19d475257a5fe1546b84
LE VU BINH <Binh9874123@gmail.com>
2021-07-31 19:43:53 +0300

    first commit

--- algoritm/test_Code.py
+++ algoritm/test_Code.py
@@ -1,12 +1,14 @@
 import pytest
 import Codethuattoan
 import sys
+import numpy as np
 
 
 @pytest.mark.example1
 def test_mode_rungeKutta():
-    sol = Codethuattoan.rungeKutta(1000, 1, 0)
-    assert [sol.S, sol.I, sol.R] == [
+    a = np.array([1000, 1, 0])
+    sol = Codethuattoan.rungeKutta(a)
+    assert [sol[0], sol[1], sol[2]] == [
         999.9989996253158,
         1.0009003371740595,
         0.00010003751011400939,
