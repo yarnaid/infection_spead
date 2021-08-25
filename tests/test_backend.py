d06cf931caa43d696f960e4b6db08d6164284fa1
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2021-08-25 18:31:24 +0300

    Added test for dummy map creation function

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -2,7 +2,9 @@ import pytest
 from backend.map_generation import ResearchMap
 from dataStructure.gRPC import Building, BaseUnit, BuildingType
 from pure_protobuf.types import int32
+from backend.map_generation import DUMMY_MAP_CONFIG_NAME, create_dummy_map
 
+# tests for basic research map functions
 no_intersection_test = [Building(int32(1), float(30), float(30), BuildingType.HOUSE, int32(20), int32(20), int32(0)),
                         Building(int32(2), float(60), float(60), BuildingType.HOUSE, int32(20), int32(20), int32(0)),
                         False,
@@ -20,3 +22,34 @@ test_examples = [has_intersection_test, no_intersection_test]
 def test_intersection_check(test_data: Building) -> None:
     first_building, second_building, answer, *_ = test_data
     assert Building.intersection_check(first_building, second_building) == answer
+
+
+# test data for dummy map
+
+
+def test_dummy_map():
+    buildings = [Building(int32(1), 108, 228, BuildingType.HOUSE, int32(72), int32(114)),
+                 Building(int32(2), 76, 288, BuildingType.HOUSE, int32(72), int32(76)),
+                 Building(int32(3), 396, 228, BuildingType.HOUSE, int32(108), int32(114)),
+                 Building(int32(4), 288, 418, BuildingType.HOUSE, int32(72), int32(76))]
+
+    dummy_map_parameters = {'map_length': 500,
+                            'map_width': 500,
+                            'buildings': 4,
+                            'population': 2,
+                            'minimal_wall_length': 10,
+                            'iteration_constraint': 1000,
+                            'indent_from_borders': 3,
+                            'wall_length_divider': 5}
+
+    test_map = create_dummy_map()
+
+    for building_required, building_created in zip(buildings, test_map.map_buildings):
+        assert building_required.coord_x == building_created.coord_x
+        assert building_required.coord_y == building_created.coord_y
+        assert building_required.type == building_created.type
+        assert building_required.length == building_created.length
+        assert building_required.width == building_created.width
+
+    for key in test_map.config_data.__dict__.keys():
+        assert dummy_map_parameters[key] == test_map.config_data.__getattribute__(key)
